dbdc0cd8a58b173d3472b6d6cab6e08586ebbe8b
BoredWasRobbed <bored7160@gmail.com>
2025-11-25 02:23:15 -0500

    New Order Removed

--- src/main/java/net/bored/common/quirks/AllForOneQuirk.java
+++ src/main/java/net/bored/common/quirks/AllForOneQuirk.java
@@ -26,7 +26,7 @@ public class AllForOneQuirk extends QuirkSystem.Quirk {
             public void onActivate(LivingEntity entity, QuirkSystem.QuirkData data, QuirkSystem.QuirkData.QuirkInstance instance) {
                 data.runtimeTags.put("AFO_MODE", "STEAL");
                 data.currentStamina -= this.getCost();
-                if(entity instanceof PlayerEntity p) p.sendMessage(Text.of("§c[AFO] Next hit will STEAL a quirk."), true);
+                if(entity instanceof PlayerEntity p) p.sendMessage(Text.of("§cNext hit will STEAL a quirk."), true);
                 this.triggerCooldown(instance);
             }
         });
@@ -36,7 +36,7 @@ public class AllForOneQuirk extends QuirkSystem.Quirk {
             public void onActivate(LivingEntity entity, QuirkSystem.QuirkData data, QuirkSystem.QuirkData.QuirkInstance instance) {
                 data.runtimeTags.put("AFO_MODE", "GIVE");
                 data.currentStamina -= this.getCost();
-                if(entity instanceof PlayerEntity p) p.sendMessage(Text.of("§e[AFO] Next hit will GIVE your selected quirk."), true);
+                if(entity instanceof PlayerEntity p) p.sendMessage(Text.of("§eNext hit will GIVE your selected quirk."), true);
                 this.triggerCooldown(instance);
             }
         });
